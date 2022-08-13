ffbcf7406c11d11f7fc4ca0934901464fe20d52a
Margarita <margarita27011991@gmail.com>
2022-08-13 17:32:11 +0300

    add task21

--- task21_massive/Program.cs
+++ task21_massive/Program.cs
@@ -1,14 +1,15 @@
 ﻿// принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве
-Console.Clear();
 Console.WriteLine("введите координаты точки A: введите xa");
-double xa = Convert.ToDouble(Console.ReadLine()); // double - тип переменной для записи значений с запятой
+double xa = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("введите ya");
 double ya = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("введите координаты точки B: введите xb");
 double xb = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("введите yb");
 double yb = Convert.ToDouble(Console.ReadLine());
-//            d = √             (хa – хb) 2      +     (уa – уb) 2) возвести в степень 2
-double distance = Math.Sqrt(Math.Pow(xa - xb, 2) + Math.Pow(ya - yb, 2));
+double[] arrayA = {xa, ya};
+double[] arrayB = {xb, yb};
+
+double distance = Math.Sqrt(Math.Pow(arrayA - arrayA, 2));
 
 Console.WriteLine($"Расстояние между точками равно {distance}");
