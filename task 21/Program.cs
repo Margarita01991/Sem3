ba8124cd96f4a4b73799accc38bbf90a9aee1fde
Margarita <margarita27011991@gmail.com>
2022-08-13 17:12:53 +0300

    add task21 расстояние между двумя точками

---
+++ task 21/Program.cs
@@ -0,0 +1,13 @@
+﻿// принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве
+Console.WriteLine("введите координаты точки A: введите xa");
+double xa = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("введите ya");
+double ya = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("введите координаты точки B: введите xb");
+double xb = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("введите yb");
+double yb = Convert.ToDouble(Console.ReadLine());
+//            d = √             (хa – хb) 2      +     (уa – уb) 2)
+double distance = Math.Sqrt(Math.Pow(xa - xb, 2) + Math.Pow(ya - yb, 2));
+
+Console.WriteLine($"Расстояние между точками равно {distance}");
