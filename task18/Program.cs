1fd68da8ec23c02ca7a75b4808ffffcea019e111
Margarita <margarita27011991@gmail.com>
2022-08-13 16:20:29 +0300

    add task18 (decision "if" and "massive")

---
+++ task18/Program.cs
@@ -0,0 +1,27 @@
+﻿// по заданному номеру четверти показывает диапазон возможных значений
+// x>0,y>0 - 1 quart, x<0,y>0 - 2 quart, x<0,y<0 - 3 quart, x>0,y<0 - 4 quart
+Console.Clear();
+Console.WriteLine("введите номер четверти ");
+
+int quart = Convert.ToInt32(Console.ReadLine());//
+
+if(quart == 1)
+{
+    Console.WriteLine("x>0,y>0");
+}
+else if(quart == 2)
+{
+    Console.WriteLine("x<0,y>0");
+}
+else if(quart == 3)
+{
+    Console.WriteLine("x<0,y<0");
+}
+else if(quart == 4)
+{
+    Console.WriteLine("x>0,y<0");
+}
+else 
+{
+     Console.WriteLine("введите номер от 1 до 4");   
+}
---
+++ task18_massive/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("введите номер четверти ");
+int quart = Convert.ToInt32(Console.ReadLine());
+string[] array = {"x > 0; y > o", "x < 0; y > ", "x < 0; y < o", "x > 0; y < o"};// записали в стринговый массив все возможные значения 
+if(quart >=1 && quart <= 4)                                                      // проверили, поставили ограничения на ввод от 1 до 4
+{
+    Console.WriteLine(array[quart-1]);                                           // -1, так как в массиве начало с 0
+}
+else
+{
+    Console.WriteLine("такой четверти нет");
+}
